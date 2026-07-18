12bf057b322c66b9abf1a22167e0108e5ee446af
Linar502Prog <LinarSSuleymanov@yandex.ru>
2026-07-18 22:01:18 +0400

    Добавьте файлы проекта.

---
+++ GreeterLibrary.cpp
@@ -0,0 +1,8 @@
+﻿#include"GreeterLibrary.h"
+#include<iostream>
+#include <format>
+namespace GreeterLibrary {
+	std::string Greeter::greet(std::string name) {
+		return std::format("Здравствуйте, {}!", name);
+	}
+}
---
+++ GreeterLibrary.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+#include<iostream>
+namespace GreeterLibrary {
+	class Greeter {
+	public:
+		std::string greet(std::string name);
+	};
+}
