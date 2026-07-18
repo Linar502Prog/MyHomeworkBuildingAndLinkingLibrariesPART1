4f6376b2d9c1f53f9d19f55bb593fa933171b56f
Linar502Prog <sullinarik@gmail.com>
2026-07-18 22:03:18 +0400

    Add files via upload

---
+++ GreeterClient.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include"GreeterLibrary.h"
+#include"Windows.h"
+int main()
+{
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    GreeterLibrary::Greeter greeter;
+    std::string name;
+    std::cout << "Введите имя: ";
+    std::cin >> name;
+    std::cout << greeter.greet(name);
+}
+
